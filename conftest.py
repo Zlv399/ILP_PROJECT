13e1cc74faf3c075dcb757b7fa29a344bb82d09f
zlv399 <86716383@qq.com>
2023-10-11 00:10:22 +0800

    fix:conftest文件增加钩子函数，支持用例断言失败自动截图并添加至allure测试报告中

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@
 @Date:2023/8/10 12:00
 """
 import os
-import pytest
+import pytest,allure
 import time
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
@@ -16,7 +16,7 @@ from config.config import IMPLICTLY_WAIT_TIMEOUT,IMAGE_PATH
 from page.login_page import LoginPage
 
 
-
+driver = None
 @pytest.fixture(scope='class')
 def browser():
     """
@@ -28,18 +28,19 @@ def browser():
     5、关闭浏览器
     :return:
     """
+    global driver
+    if driver == None:
     #解决ssl协议握手报错ERROR:ssl_client_socket_impl.cc(962)] handshake failed；returned -1, SSL error code 1
-    chrome_options = Options()
-    #
-    chrome_options.add_argument('--disable-extensions')
-    chrome_options.add_experimental_option('excludeSwitches', ['ignore-certificate-errors'])
+        chrome_options = Options()
+        chrome_options.add_argument('--disable-extensions')
+        chrome_options.add_experimental_option('excludeSwitches', ['ignore-certificate-errors'])
     #将窗口最大化作为chrome的启动选项，即启动浏览器时就是最大化
-    chrome_options.add_argument('--start-maximized')
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.implicitly_wait(IMPLICTLY_WAIT_TIMEOUT)
+        chrome_options.add_argument('--start-maximized')
+        driver = webdriver.Chrome(options=chrome_options)
+        driver.implicitly_wait(IMPLICTLY_WAIT_TIMEOUT)
     # driver.maximize_window()
-    yield driver
-    driver.quit()
+        yield driver
+        driver.quit()
 
 @pytest.fixture(scope='class')
 def login(browser):
@@ -75,3 +76,31 @@ def pytest_collection_modifyitems(items):
         item.name = item.name.encode("utf-8").decode("unicode_escape")
         # print(item.nodeid)
         item._nodeid = item.nodeid.encode("utf-8").decode("unicode_escape")
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item):
+    """
+    hook pytest allure用例失败截图
+    :param item:
+    :return:
+    """
+    # 获取钩子方法的调用结果
+    outcome = yield
+    # 从钩子方法的调用结果中获取测试报告
+    rep = outcome.get_result()
+    # print(rep,'调试')
+    # rep.when表示测试步骤，仅仅获取用例call 执行结果是失败的情况, 不包含 setup/teardown
+    if rep.when == "call" and rep.failed:
+        mode = "a" if os.path.exists("failures") else "w"
+        with open("failures", mode) as f:
+            if "tmpdir" in item.fixturenames:
+                extra = " (%s)" % item.funcargs["tmpdir"]
+            else:
+                extra = ""
+            f.write(rep.nodeid + extra + "\n")
+        # 添加报告截图
+        with allure.step('查看失败截图'):
+             #需要先实例化一个driver，才能用到driver的截图方法
+            allure.attach(driver.get_screenshot_as_png(), "失败截图",
+            allure.attachment_type.PNG)
