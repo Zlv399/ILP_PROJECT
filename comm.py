b861fa5a98a42bac38a1845cc1d10bd142ff41b6
fang <86716383@qq.com>
2023-12-12 02:18:22 +0800

    fix:维护本地

--- comm.py
+++ comm.py
@@ -26,3 +26,35 @@
 #             raise FileNotFoundError("%s 文件不存在！" % self.file_path)
 #         with open(self.file_path, encoding='utf-8') as f:
 #             self.data = yaml.safe_load(f)
+
+import pytest
+
+class Authenticator:
+    def username_password_authenticate(self, username, password):
+        # 模拟用户名密码认证逻辑
+        if username == "user1" and password == "pass123":
+            return True
+        else:
+            return False
+
+    def social_media_authenticate(self, social_name, social_type):
+        # 模拟社交媒体认证逻辑
+        if social_name == "zhangsan" and social_type == 'Tiktok':
+            return True
+        else:
+            return False
+
+@pytest.fixture
+def authentication_method(request):
+    # 根据参数选择认证方式
+    method = request.param
+    if method == 'username_password':
+        return Authenticator().username_password_authenticate
+    elif method == 'social_media':
+        return Authenticator().social_media_authenticate
+
+@pytest.mark.parametrize('authentication_method', ['username_password','social_media'],indirect=True)
+def test_login(authentication_method):
+    # 在测试函数中使用 authentication_method 进行测试
+    result = authentication_method('user1', 'pass123')
+    assert result is True
